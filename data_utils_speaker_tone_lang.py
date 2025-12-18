efb5e0ef8ef8739c30fcc2b8f049e56f2dfca8ca
kenenbek <kenenbek@gmail.com>
2025-12-18 20:03:44 +0600

    w

--- data_utils_speaker_tone_lang.py
+++ data_utils_speaker_tone_lang.py
@@ -79,7 +79,6 @@ class TextAudioSpeakerToneLangLoader(torch.utils.data.Dataset):
         accepted_count = 0
 
         for audiopath, sid, tone_id, lid, real_text, text in self.audiopaths_sid_tone_lang_text:
-            print(audiopath)
             text_len = len(text)
 
             # Check text length
