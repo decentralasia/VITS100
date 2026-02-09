3c6bd068a92c60f143b11398d3a963827ca2e67d
kenenbek <kenenbek@gmail.com>
2026-02-09 10:53:07 +0500

    m

--- data_utils_speaker_tone_lang.py
+++ data_utils_speaker_tone_lang.py
@@ -472,7 +472,8 @@ def print_random_phonemized_samples(manifest_path: str, num_samples: int = 32):
         # Replace paralinguistic tags with special symbols
         for tag, symbol in MAPPING_SOUND.items():
             text = text.replace(tag, symbol)
-        
+
+        print("kokoko", text)
         if is_kyrgyz:
             phonemized = KYRGYZ_PHONEMIZER.phonemize(text)
             phonemized = collapse_whitespace(phonemized)
