18c773efe5ab74c55b68f425c87c4e080c85f761
kenenbek <kenenbek@gmail.com>
2026-03-30 15:00:41 +0500

    f

--- commons.py
+++ commons.py
@@ -50,7 +50,8 @@ def slice_segments(x, ids_str, segment_size=4):
   for i in range(x.size(0)):
     idx_str = ids_str[i]
     idx_end = idx_str + segment_size
-    ret[i] = x[i, :, idx_str:idx_end]
+    actual = x[i, :, idx_str:idx_end]
+    ret[i, :, :actual.size(-1)] = actual
   return ret
 
 
@@ -58,7 +59,7 @@ def rand_slice_segments(x, x_lengths=None, segment_size=4):
   b, d, t = x.size()
   if x_lengths is None:
     x_lengths = t
-  ids_str_max = x_lengths - segment_size + 1
+  ids_str_max = torch.clamp(x_lengths - segment_size + 1, min=1)
   ids_str = (torch.rand([b]).to(device=x.device) * ids_str_max).to(dtype=torch.long)
   ret = slice_segments(x, ids_str, segment_size)
   return ret, ids_str
